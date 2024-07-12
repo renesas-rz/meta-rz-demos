4a39ac5f097f0e7a53a06766b9df8ed59ed06f0f
Yoshihito Ogawa <yoshihito.ogawa.kc@renesas.com>
2024-07-12 14:00:48 +0900

    benchmark-demo: update RZ Linux benchmark demo script files for video test
    Signed-off-by: Yoshihito Ogawa <yoshihito.ogawa.kc@renesas.com>

--- recipes-graphics/benchmark-demo/benchmark-demo-init/video-dec.sh
+++ recipes-graphics/benchmark-demo/benchmark-demo-init/video-dec.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-tmp=$(mktemp)
+moviefile="/usr/share/movies/big_buck_bunny_1080p_30fps_30s.mp4"
 
-gst-launch-1.0 -r filesrc location=/usr/share/movies/big_buck_bunny_1080p.mp4 ! qtdemux ! queue ! h264parse ! omxh264dec ! waylandsink max-lateness=-1 qos=false sync=false name=v -p v:sink >> $tmp
+tmp1=$(mktemp)
+tmp2=$(mktemp)
+
+top -d 1 -b > $tmp1 &
+
+gst-launch-1.0 -r filesrc location=${moviefile} ! qtdemux ! h264parse ! omxh264dec ! waylandsink max-lateness=-1 qos=false name=v -rp v:sink > $tmp2
+
+kill -9 top
+sleep 1
+
+# output result of top (CPU usage)
+grep -m 1 "COMMAND" $tmp1
+grep $moviefile $tmp1
 
 # remove control characters in the result
-tr -d '\0' < $tmp
+tr -d '\0' < $tmp2
 
-rm $tmp
+rm $tmp1
+rm $tmp2
