13a41f17c7752d7fc60349c2fab1e919220a3944
Yoshihito Ogawa <yoshihito.ogawa.kc@renesas.com>
2026-07-17 19:04:42 +0900

    demo-launcher: update Flutter application paths
    Update launcher scripts to use the new application paths
    introduced by the Flutter 3.38.3 upgrade.
    
    Signed-off-by: Yoshihito Ogawa <yoshihito.ogawa.kc@renesas.com>

--- recipes-apps/demo-launcher/demo-launcher/flutter_deeplink_store_demo.sh
+++ recipes-apps/demo-launcher/demo-launcher/flutter_deeplink_store_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export LD_PRELOAD=/usr/share/flutter/3.27.1/release/lib/libflutter_engine.so
+export LD_PRELOAD=/usr/share/flutter/3.38.3/release/lib/libflutter_engine.so
 
-flutter-client -f -b /usr/share/flutter/flutter-samples-deeplink-store-example/3.27.1/release
+flutter-client -f -b /usr/share/flutter/flutter-samples-deeplink-store-example/3.38.3/release
